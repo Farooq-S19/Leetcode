7a8a156612bc5cafb7cd21c3c6eee7a582b0d96e
Farooq-S19 <shaikfarooq9392@gmail.com>
2026-07-29 12:12:31 +0530

    Time: 1 ms (61.41%), Space: 20.8 MB (6.78%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,15 +1,7 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        store = {}
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hash = {}
         for i in range(len(nums)):
-            if nums[i] in store:
-                return store[nums[i]],i
-            else:
-                store[target - nums[i]] =i
-        
-        
+            if nums[i] in hash:
+                return [hash[nums[i]],i]
+            hash[target-nums[i]]=i
